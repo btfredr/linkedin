b5b530e42ab7b342aba6c7ee841f8be8bd7dbc9b
Fredrik <fredrikf2000@gmail.com>
2021-07-01 19:34:34 +0200

    made and styled header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,11 @@
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -19,8 +24,15 @@ function Header() {
       </div>
 
       <div className="header__right">
-        <HeaderOption title="Home" />
-        <HeaderOption title="My Network" />
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          title="Me"
+          avatar="https://media-exp3.licdn.com/dms/image/C4D03AQH_7IhaTwnACw/profile-displayphoto-shrink_800_800/0/1610539044747?e=1630540800&v=beta&t=nJcfu97eU9-BsIbxszo5O-Mi2o-hQlu1unxm4BQcRJU"
+        />
       </div>
     </div>
   );
