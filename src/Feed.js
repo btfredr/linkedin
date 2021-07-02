40a939fb55f60c83aca9de0fd94f288b8d3dfbb8
Fredrik <fredrikf2000@gmail.com>
2021-07-02 20:44:19 +0200

    started working on post center

--- src/Feed.js
+++ src/Feed.js
@@ -1,16 +1,34 @@
-import CreateIcon from "@material-ui/icons/Create";
 import "./Feed.css";
+import InputOption from "./InputOption";
+import {
+  CalendarViewDay,
+  EventNote,
+  Subscriptions,
+  Image,
+  Create,
+} from "@material-ui/icons";
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
-          <CreateIcon />
+          <Create />
           <form>
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+
+        <div className="feed__inputOptions">
+          <InputOption Icon={Image} title="Photo" color="#70b5f9" />
+          <InputOption Icon={Subscriptions} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNote} title="Event" color="#c0cbcd" />
+          <InputOption
+            Icon={CalendarViewDay}
+            title="Write article"
+            color="#7fc15e"
+          />
         </div>
       </div>
     </div>
