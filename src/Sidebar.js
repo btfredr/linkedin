20b4a9c240656bff4cb622076780850312a442fd
Fredrik <fredrikf2000@gmail.com>
2021-07-02 11:05:46 +0200

    started working on sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
