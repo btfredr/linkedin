40ba7a50213ef063bb791854ee3da9d4abd6e3df
Fredrik <fredrikf2000@gmail.com>
2021-07-02 12:12:31 +0200

    styling feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,18 @@
+import CreateIcon from "@material-ui/icons/Create";
 import "./Feed.css";
 
 function Feed() {
   return (
-    <div>
-      <div className="feed__inputContainer"></div>
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
