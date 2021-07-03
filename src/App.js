506f7d8c87ac9c1ad197a4198fc7b27a85ec4997
Fredrik <fredrikf2000@gmail.com>
2021-07-03 15:24:29 +0200

    started working on login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
+import Login from "./Login";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
